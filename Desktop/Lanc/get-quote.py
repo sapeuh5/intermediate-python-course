3e7fc242f7729d698643b2b2342559d7189d06ca
briceno <briceno@outlook.fr>
2021-03-28 00:14:41 +0100

    Tutorial complete

--- Desktop/Lanc/get-quote.py
+++ Desktop/Lanc/get-quote.py
@@ -1,14 +1,18 @@
 def kika():
    import random
-   last=13
-   rnd=random.randint(0,last)
+   
    #print("Keep it logically awesome.")
-
+   f=open("quotes.txt","a")
+   f.write("Et cela c'est la fin\n")
+   f.close()
    f = open("quotes.txt")
    quotes = f.readlines()
    f.close()
-
-   print(quotes[rnd])
+   last=len(quotes)-1
+   rnd=random.randint(0,last)
+   rnd1=random.randint(0,last)
+   print(quotes[rnd],end='')
+   print(quotes[rnd1],end='')
 
 if __name__== "__main__":
   kika()
